d1d545eba394b869285036525a2993c53dec47f1
Lucas Henriques <lucashenriques@localhost.localdomain>
2021-06-06 13:05:03 -0300

    Exemplos de testing-library

---
+++ testing-library/README.md
@@ -0,0 +1,5 @@
+# Testes com Testing-library no React
+
+## Projeto baseado nesse artigo aqui
+https://blog.rocketseat.com.br/introducao-a-testing-library-testando-componentes-react/
+
---
+++ testing-library/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Technologies from './pages/Technologies'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bem vindo aos testes</h1>
+      <Technologies />
+    </div>
+  );
+}
+
+export default App;
